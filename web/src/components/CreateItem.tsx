96efb7aef2bb08793be47da1de4a9401e82e8f1f
inaveentata <inaveentata@outlook.com>
2022-12-15 22:58:19 +0530

    Add error prop from mantine

--- web/src/components/CreateItem.tsx
+++ web/src/components/CreateItem.tsx
@@ -8,13 +8,14 @@ import {
   Text,
   Textarea,
   Flex,
+  Input,
 } from "@mantine/core";
 import { IconSend } from "@tabler/icons";
 
 const ItemDataSchema = z.object({
   name: z.string().min(1, { message: "Item name is required" }),
   price: z.string().min(1, { message: "Item price is required" }),
-  availableQuantity: z.string(),
+  availableQuantity: z.string().min(1,{message:'Available Qty is required'}),
   allowedMinQuantity: z
     .string()
     .min(1, { message: "Allowed min. quantity is 1" }),
@@ -38,7 +39,6 @@ function CreateItem() {
 
   console.log(errors);
 
-
   const onSubmit = (data: ItemData) => {
     console.log(data);
     // const reader = new FileReader()
@@ -57,107 +57,57 @@ function CreateItem() {
           justify="center"
           sx={{ width: 700, padding: "2rem", fontSize: "1.2rem" }}
         >
-          <TextInput
-            label="Name"
-            placeholder="Eg: laptop"
-            id="name"
-            {...register("name")}
-          />{" "}
-          {errors.name && (
-            <small
-              style={{
-                fontStyle: "italic",
-                fontSize: "16px",
-                color: "red",
-                marginTop: "4px",
-              }}
-            >
-              {errors.name?.message}
-            </small>
-          )}
-          <TextInput
-            label="Price"
-            placeholder="Eg: Rs.50000"
-            id="price"
-            {...register("price")}
-          />{" "}
-          {errors.price && (
-            <small
-              style={{
-                fontStyle: "italic",
-                fontSize: "16px",
-                color: "red",
-                marginTop: "4px",
-              }}
-            >
-              {errors.price?.message}
-            </small>
-          )}
-          <TextInput
-            label="Avl.Quantity"
-            placeholder="Eg: 100"
+          <Input.Wrapper id="name" label="Name" error={errors.name?.message}>
+            <Input id="name" placeholder="Eg: laptop" {...register("name")} />
+          </Input.Wrapper>
+          <Input.Wrapper id="price" label="Price" error={errors.price?.message}>
+            <Input id="price" placeholder="Eg: 10000" {...register("price")} />
+          </Input.Wrapper>
+          <Input.Wrapper
             id="availableQuantity"
-            {...register("availableQuantity")}
-          />{" "}
-          {errors.availableQuantity && (
-            <small
-              style={{
-                fontStyle: "italic",
-                fontSize: "16px",
-                color: "red",
-                marginTop: "4px",
-              }}
-            >
-              {errors.availableQuantity?.message}
-            </small>
-          )}
-          <TextInput
-            label="Min.Qty"
-            placeholder="Eg: 1"
+            label="Avl.Quantity"
+            error={errors.availableQuantity?.message}
+          >
+            <Input
+              id="availableQuantity"
+              placeholder="Eg: 140"
+              {...register("availableQuantity")}
+            />
+          </Input.Wrapper>
+          <Input.Wrapper
             id="allowedMinQuantity"
-            {...register("allowedMinQuantity")}
-          />{" "}
-          {errors.allowedMinQuantity && (
-            <small
-              style={{
-                fontStyle: "italic",
-                fontSize: "16px",
-                color: "red",
-                marginTop: "4px",
-              }}
-            >
-              {errors.allowedMinQuantity?.message}
-            </small>
-          )}
-          <TextInput
-            label="Max.Qty"
-            max={5}
-            placeholder="Eg: 5"
+            label="Min.Qty"
+            error={errors.allowedMinQuantity?.message}
+          >
+            <Input
+              id="allowedMinQuantity"
+              placeholder="Eg: 1"
+              {...register("allowedMinQuantity")}
+            />
+          </Input.Wrapper>
+          <Input.Wrapper
             id="allowedMaxQuantity"
-            {...register("allowedMaxQuantity")}
-          />{" "}
-          {errors.allowedMaxQuantity && (
-            <small
-              style={{
-                fontStyle: "italic",
-                fontSize: "16px",
-                color: "red",
-                marginTop: "4px",
-              }}
-            >
-              {errors.allowedMaxQuantity?.message}
-            </small>
-          )}
-          <Stack spacing={0}>
+            label="Max.Qty"
+            error={errors.allowedMaxQuantity?.message}
+          >
+            <Input
+              id="allowedMaxQuantity"
+              placeholder="Eg: 5"
+              {...register("allowedMaxQuantity")}
+            />
+          </Input.Wrapper>
 
-          <label htmlFor="image" style={{fontSize:'1rem'}}>Image</label>
-          <input
-            placeholder="Upload Image"
-            type="file"
-            accept="image/*"
-            id="image"
-            {...register("image")}
-          />
+          <Stack spacing={0}>
+            <label htmlFor="image" style={{ fontSize: "1rem" }}>
+              Image
+            </label>
+            <input
+              placeholder="Upload Image"
+              type="file"
+              accept="image/*"
+              id="image"
+              {...register("image")}
+            />
           </Stack>
           <Textarea
             placeholder="About the item"
