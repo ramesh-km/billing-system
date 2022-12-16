fa2ec0d9a8ce6cb099843f6893ae5957cb4d9c53
Ramesh <ramesh-km@outlook.com>
2022-12-16 22:04:44 +0530

    Add cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -4,10 +4,12 @@ import helmet from "helmet";
 import itemsRouter from "./items/items.router";
 import customersRouter from "./customers/customers.router";
 import errorMiddleware from "./lib/error.middleware";
+import cors from "cors";
 
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
