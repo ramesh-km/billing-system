ff2f67569222ef7ff65c6b57741b751cf89f9bf7
Ramesh <ramesh-km@outlook.com>
2022-12-19 18:40:00 +0530

    Disable refetch on focus change

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -3,7 +3,13 @@ import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import router from "./routes/routes";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 function App() {
   return (
