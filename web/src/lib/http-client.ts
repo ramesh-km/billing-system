c8e9d2b59f3e3cda025027d9efe2211fbf6dcf65
inaveentata <inaveentata@outlook.com>
2022-12-22 11:36:37 +0530

    Add Bearer token

--- web/src/lib/http-client.ts
+++ web/src/lib/http-client.ts
@@ -1,7 +1,19 @@
 import axios from "axios";
+import { deserialize } from "../contexts/AuthProvider";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API,
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token && config.headers) {
+      config.headers.Authorization = `Bearer ${deserialize(token)}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
